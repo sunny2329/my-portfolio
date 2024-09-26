6a4476b19495cb90ce278a1b0dc14fb4b0c313cc
Sunny <sobhit230@gmail.com>
2024-09-27 01:21:44 +0530

    About Section Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,61 @@
-
-
-
-export default function Home() {
+'use client'
+import { motion } from "framer-motion";
+import { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next";
+import { fadeInUp, routeFade, stagger } from "../animations";
+import ServiceCard from "../components/ServiceCard";
+import { services } from "../data";
+import Head from "next/head";
+const About = () => {
 
   return (
-    <>
+    <motion.div
+      className="flex flex-col flex-grow px-6 pt-1 "
+      variants={routeFade}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <Head>
+        <title>Sumit Dey | Web Developer</title>
+      </Head>
+      {/* // h5 */}
+      <h4 className="my-3">
+        I am currently pursuing B.Tech Degree(Final Year) in Computer Science
+        Engineering from Academy of Technology. I have 3+ years of experience in
+        Web Development and I have a Youtube Channel where I teach Full Stack
+        Web Development
+      </h4>
+      <div
+        className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-black-100"
+        style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
+      >
+        <h4 className="my-3 text-xl font-semibold tracking-wide text-gray-900">
+          What I am doing
+        </h4>
 
-      <h1>Hello World</h1>
-    </>
+        <motion.div
+          className="grid gap-6 my-3 md:grid-cols-2"
+          variants={stagger}
+          animate="animate"
+          initial="initial"
+        >
+          {/* children's initial and animate property should be same as the parent during a stagger effect  */}
+          {services.map((service) => (
+            <motion.div
+              className="col-span-2 p-2 bg-gray-200 rounded-lg dark:bg-black-200 md:col-span-1 "
+              key={service.title}
+              variants={fadeInUp}
+              // animate="animate"
+              // initial="initial"
+            >
+              <ServiceCard service={service} />
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </motion.div>
   );
-}
+};
+
+
+export default About;
