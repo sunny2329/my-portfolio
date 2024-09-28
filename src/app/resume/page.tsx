fa1d8979242b7192dca0e7a4a2024250fbd9ff68
Sunny <sobhit230@gmail.com>
2024-09-28 10:22:06 +0530

    Bars Corrected

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { fadeInUp, routeFade } from "../../animations";
 import Bar from "../../components/Bar";
 import { languages, tools } from "../../data";
+import { WobbleCard } from "@/components/ui/wobble-card";
 
 const Resume = () => {
     return (
@@ -20,25 +21,31 @@ const Resume = () => {
             {/* //! Education & Experience */}
             <div className="grid gap-6 md:grid-cols-2">
                 <motion.div variants={fadeInUp} initial="initial" animate="animate">
-                    <h5 className="my-3 text-2xl font-bold">Education</h5>
-                    <div className="">
-                        <h5 className="my-2 text-xl font-bold">
-                            Electronics and Communication Engineering
-                        </h5>
-                        <p className="font-semibold">The LNM Institute of Technology (2021-2025)</p>
-                        <p className="my-3">
-                            I am currently pursuing B.tech in Electronics and Communication Engineering
-                            from The LNMIIT
-                        </p>
-                    </div>
+                    <WobbleCard>
+                        <h5 className="my-3 text-2xl font-bold">Education</h5>
+                        <div className="">
+                            <h5 className="my-2 text-xl font-bold">
+                                Electronics and Communication Engineering
+                            </h5>
+                            <p className="font-semibold">The LNM Institute of Technology (2021-2025)</p>
+                            <p className="my-3">
+                                I am currently pursuing B.tech in Electronics and Communication Engineering
+                                from The LNMIIT
+                            </p>
+                        </div>
+                    </WobbleCard>
+
                 </motion.div>
                 <motion.div variants={fadeInUp} initial="initial" animate="animate">
-                    <h5 className="my-3 text-2xl font-bold">Experience</h5>
-                    <div>
-                        <h5 className="my-2 text-xl font-bold">ReactJS Developer Intern</h5>
-                        <p className="font-semibold">Codehop Interfusion Limited</p>
-                        <p className="my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim iste laboriosam ipsam cupiditate repellat fuga quod eligendi necessitatibus porro, quas qui, corrupti voluptas?</p>
-                    </div>
+                    <WobbleCard>
+                        <h5 className="my-3 text-2xl font-bold">Experience</h5>
+                        <div>
+                            <h5 className="my-2 text-xl font-bold">ReactJS Developer Intern</h5>
+                            <p className="font-semibold">Codehop Interfusion Limited</p>
+                            <p className="my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim iste laboriosam ipsam cupiditate repellat fuga quod eligendi necessitatibus porro, quas qui, corrupti voluptas?</p>
+                        </div>
+                    </WobbleCard>
+
                 </motion.div>
             </div>
 
--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -23,9 +23,9 @@ const Bar: FunctionComponent<{ value: ISkill }> = ({
     };
 
     return (
-        <div className="my-2 text-white bg-gray-300 rounded-full dark:bg-black-300">
+        <div className="my-2 text-white bg-green-500 rounded-full dark:bg-black-300">
             <motion.div
-                className="flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-green to-blue-500"
+                className="flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-green-700 to-blue-500"
                 style={{
                     width: bar_width,
                 }}
